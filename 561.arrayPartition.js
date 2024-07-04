b2d970a4f97412f830282f21887529c2931a4d6c
iqbalmuhmd <iqbxlmhd@gmail.com>
2024-07-04 10:02:00 +0530

    Array Partition

---
+++ 561.arrayPartition.js
@@ -0,0 +1,15 @@
+/**
+ * Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2),
+ * ..., (an, bn) such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+const arrayPairSum = function (nums) {
+  nums.sort((a, b) => a - b);
+  let sum = 0;
+  for (let i = 0; i < nums.length; i += 2) {
+    sum += nums[i];
+  }
+  return sum;
+};
